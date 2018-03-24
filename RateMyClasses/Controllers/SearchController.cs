575c14014476339978cf25070128132c8c73db51
Adam Beck <akb93@case.edu>
2018-03-24 16:05:06 -0400

    Add search button to shared bar

--- RateMyClasses/Controllers/SearchController.cs
+++ RateMyClasses/Controllers/SearchController.cs
@@ -1,7 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RateMyClasses.Models;
+
 namespace RateMyClasses.Controllers {
-	public class SearchController {
-		public SearchController() {
+	public class SearchController: Controller {
+		public IActionResult Index() {
+			ViewData["Title"] = "Search";
+			ViewData["Message"] = "Enter in a course to search";
+			return View();
 		}
+
+		public IActionResult Error() {
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
 	}
 }
