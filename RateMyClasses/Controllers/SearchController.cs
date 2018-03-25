9972e9ac3eefcf7bf0aa3627de1c0fa1a5e171bc
Adam Beck <akb93@case.edu>
2018-03-25 02:20:55 -0400

    Add search button functionality for search page

--- RateMyClasses/Controllers/SearchController.cs
+++ RateMyClasses/Controllers/SearchController.cs
@@ -5,13 +5,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RateMyClasses.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RateMyClasses.Models;
 
 namespace RateMyClasses.Controllers {
 	public class SearchController: Controller {
-		public IActionResult Index() {
+
+		private readonly CourseContext _context;
+
+		public SearchController(CourseContext context) {
+			_context = context;
+		}
+
+		public ActionResult Index(string searchString = "") {
 			ViewData["Title"] = "Search";
 			ViewData["Message"] = "Enter in a course to search";
-			return View();
+
+			var allCourses = from c in _context.Course
+							 select c;
+
+			var noCourses = allCourses.Where(i => i.id == 0);
+
+			if (String.IsNullOrEmpty(searchString)) {
+				return View(noCourses);
+			}
+
+			else {
+				var selectedCourses = allCourses.Where(c => c.name.ToLower().Contains(searchString.ToLower()));
+				return View(selectedCourses);
+			}
 		}
 
 		public IActionResult Error() {
