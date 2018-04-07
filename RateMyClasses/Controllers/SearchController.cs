5893acf2a627981787cd9e82cb39f804cad2adc4
Adam Beck <akb93@case.edu>
2018-04-07 17:27:11 -0400

    Reformat old formatted code

--- RateMyClasses/Controllers/SearchController.cs
+++ RateMyClasses/Controllers/SearchController.cs
@@ -16,18 +16,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RateMyClasses.Models;
 
-namespace RateMyClasses.Controllers {
-	public class SearchController: Controller {
+namespace RateMyClasses.Controllers
+{
+	public class SearchController: Controller
+	{
 
 		private readonly CourseContext _context;
 
-		public SearchController(CourseContext context) {
+		public SearchController(CourseContext context)
+		{
 			_context = context;
 		}
 
 		public ActionResult Index(string nameString = "",
 								  string departmentString = "",
-								  string descriptionString = "") { // now keyword
+								  string descriptionString = "")
+		{ // now keyword
 			ViewData["Title"] = "Search";
 			ViewData["Message"] = "Enter in a course to search";
 
@@ -37,18 +41,21 @@ namespace RateMyClasses.Controllers {
 			var noCourses = allCourses.Where(i => i.id == 0);
 
 			// search by name
-			if (!String.IsNullOrEmpty(nameString)) {
+			if (!String.IsNullOrEmpty(nameString))
+			{
 				selectedCourses = selectedCourses.Where(c => c.name.ToLower().Contains(nameString.ToLower()));
 			}
 
 			// search by department
-			if (!String.IsNullOrEmpty(departmentString)) {
+			if (!String.IsNullOrEmpty(departmentString))
+			{
 				selectedCourses = selectedCourses.Where(c => c.department.ToLower().Contains(departmentString.ToLower()));
 
 			}
 
 			// search by keyword, which is description and name
-			if (!String.IsNullOrEmpty(descriptionString)) {
+			if (!String.IsNullOrEmpty(descriptionString))
+			{
 				selectedCourses = selectedCourses.Where(c => (c.description.ToLower().Contains(descriptionString.ToLower())) ||
 														(c.name.ToLower().Contains(descriptionString.ToLower())));
 			}
@@ -60,12 +67,14 @@ namespace RateMyClasses.Controllers {
 			// if there was no search specified, return no courses
 			if (String.IsNullOrEmpty(nameString) &&
 				String.IsNullOrEmpty(departmentString) &&
-				String.IsNullOrEmpty(descriptionString)) {
+				String.IsNullOrEmpty(descriptionString))
+			{
 
 				return View(noCourses);
 			}
 
-			else {
+			else
+			{
 				return View(selectedCourses);
 			}
 		}
--- RateMyClasses/Models/Student.cs
+++ RateMyClasses/Models/Student.cs
@@ -3,9 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RateMyClasses.Models
 {
-    public class Student
-    {
-        public Int64 id { get; set; }
+	public class Student
+	{
+		public Int64 id { get; set; }
 
 		[StringLength(60, MinimumLength = 2)]
 		[DataType(DataType.Text)]
@@ -15,21 +15,21 @@ namespace RateMyClasses.Models
 		[StringLength(60, MinimumLength = 2)]
 		[DataType(DataType.Text)]
 		[Required]
-        public string lastName { get; set; }
-        
+		public string lastName { get; set; }
+
 		[DataType(DataType.Text)]
 		[StringLength(60, MinimumLength = 2)]
 		[Required]
 		public string studentStatus { get; set; }
-        
+
 		[StringLength(30, MinimumLength = 6)]
 		[Required]
 		public string userName { get; set; }
-        
+
 		public bool isModerator { get; set; }
-        
+
 		public Student()
-        {
-        }
-    }
+		{
+		}
+	}
 }
