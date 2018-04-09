86a03e77213ffb48ebb4379f3d7381377dad2ff1
ddo3 <ddo3@case.edu>
2018-04-08 21:41:01 -0400

    first attempt at using moq. not working

--- RateMyClassesTester/ControllerTester.cs
+++ RateMyClassesTester/ControllerTester.cs
@@ -2,76 +2,122 @@
 using RateMyClasses.Controllers;
 using RateMyClasses.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
-using System.Text;
-using System.IO;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.ViewEngines;
-using System;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using Moq;
+
 
 namespace RateMyClassesTester
 {
-	public class ControllerTester
-	{
+    public class ControllerTester
+    {
 
-		// SearchController
-		[Fact]
-		public void Empty_Search_Query() 
-		{
-			CourseContext c = new CourseContext(new DbContextOptions<CourseContext>());
-			var result = new SearchController(c).Index() as ViewResult;
-			var viewData = result.ViewData;
+        // SearchController
+        [Fact]
+        public void Empty_Search_Query()
+        {
+            CourseContext c = new CourseContext(new DbContextOptions<CourseContext>());
+            var result = new SearchController(c).Index() as ViewResult;
+            var viewData = result.ViewData;
 
-			Assert.True(viewData["Result"].Equals("0"));
-		}
+            Assert.True(viewData["Result"].Equals("0"));
+        }
 
-		[Fact]
-		public void One_Or_More_Search_Query()
-		{
-			CourseContext c = new CourseContext(new DbContextOptions<CourseContext>());
-			var result = new SearchController(c).Index("132", "", "") as ViewResult;
-			var viewData = result.ViewData;
+        [Fact]
+        public void One_Or_More_Search_Query()
+        {
+            CourseContext c = new CourseContext(new DbContextOptions<CourseContext>());
+            var result = new SearchController(c).Index("132", "", "") as ViewResult;
+            var viewData = result.ViewData;
 
-			Assert.True(viewData["Result"].Equals("1"));
-		}
-
-
-		// ReviewController
-		[Fact]
-		public void No_Reviews_Filter()
-		{
-			ReviewContext c = new ReviewContext(new DbContextOptions<ReviewContext>());
-			var result = new ReviewController(c).FilterBy() as ViewResult;
-			var viewData = result.ViewData;
-
-			Assert.True(viewData["Result"].Equals("0"));
-		}
-
-		// ModeratorController
-		[Fact]
-		public void All_Reviews_Listed_Moderator()
-		{
-			ReviewContext a = new ReviewContext(new DbContextOptions<ReviewContext>());
-			ReportContext b = new ReportContext(new DbContextOptions<ReportContext>());
-
-			var result = new ModeratorController(b, a).Index() as ViewResult;
-			var viewData = result.ViewData;
+            Assert.True(viewData["Result"].Equals("1"));
+        }
 
 
-			Assert.True(!viewData["Result"].Equals("example"));
-		}
+        // ReviewController
+        [Fact]
+        public void No_Reviews_Filter()
+        {
+            ReviewContext c = new ReviewContext(new DbContextOptions<ReviewContext>());
+            var result = new ReviewController(c).FilterBy() as ViewResult;
+            var viewData = result.ViewData;
 
-	}
+            Assert.True(viewData["Result"].Equals("0"));
+        }
 
+        // ModeratorController
+        [Fact]
+        public void All_Reviews_Listed_Moderator()
+        {
+            ReviewContext a = new ReviewContext(new DbContextOptions<ReviewContext>());
+            ReportContext b = new ReportContext(new DbContextOptions<ReportContext>());
+
+            var result = new ModeratorController(b, a).Index() as ViewResult;
+            var viewData = result.ViewData;
+
+
+            Assert.True(!viewData["Result"].Equals("example"));
+        }
+
+
+        [Fact]
+        public void TestingMock()
+        {
+
+            //create a list of review 
+            var r1 = new Review();
+            r1.courseId = 1;
+            r1.description = "cool cool";
+
+
+            var r2 = new Review();
+            r2.courseId = 2;
+            r2.description = "super hard";
+
+
+            var r3 = new Review();
+            r3.courseId = 3;
+            r3.description = "very easy";
+
+
+            var r4 = new Review();
+            r4.courseId = 4;
+            r4.description = "ruined my life";
+
+
+            var reviews = new List<Review>{
+                r1,r2,r3,r4,
+
+            }.AsQueryable();
+
+
+            var reviewMock = new Mock<DbSet<Review>>();
+            reviewMock.As<IQueryable<Review>>().Setup(m => m.Provider).Returns(reviews.Provider);
+            reviewMock.As<IQueryable<Review>>().Setup(m => m.Expression).Returns(reviews.Expression);
+            reviewMock.As<IQueryable<Review>>().Setup(m => m.ElementType).Returns(reviews.ElementType);
+            reviewMock.As<IQueryable<Review>>().Setup(m => m.GetEnumerator()).Returns(reviews.GetEnumerator());
+
+
+            var reviewContextMock = new Mock<ReviewContext>();
+            //var reviewContextMock = new Mock<IModel>();
+            reviewContextMock.Setup(x => x.Review).Returns(reviewMock.Object);
+
+            var test = reviewContextMock.Object;
+
+            //var rc = test.GetContext();
+
+            var controller = new ReviewController(test);
+
+            var result = controller.FilterBy() as ViewResult;
+
+            var viewData = result.ViewData;
+
+
+            Assert.True(!viewData["Result"].Equals("example"));
+
+        }
+
+    }
 
 }
