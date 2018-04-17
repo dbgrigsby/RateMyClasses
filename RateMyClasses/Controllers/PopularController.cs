7ca0a9b34f1f252202e6d05fe422c69b0ac29fb1
Adam Beck <akb93@case.edu>
2018-04-17 12:22:36 -0400

    Add popular page for reviews

---
+++ RateMyClasses/Controllers/PopularController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RateMyClasses.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RateMyClasses.Controllers
+{
+	public class PopularController: Controller
+	{
+
+		private readonly ReviewContext _context;
+
+		public PopularController(ReviewContext context)
+		{
+			_context = context;
+		}
+
+		public ActionResult Index()
+		{
+			ViewData["Title"] = "Highest Rated Professors";
+			ViewData["Message"] = "Here are reviews with the highest scores.";
+
+			var allReviews = from r in _context.Review
+							 where r.score >= 8
+							 select r;
+
+			return View(allReviews);
+		}
+	}
+}
