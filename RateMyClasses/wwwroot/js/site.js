2713894828c8d3eca9eb8c63c05db5acf3d53534
Daniel Grigsby <dbg28@case.edu>
2018-04-19 13:07:32 -0400

    Fix spacing on professor info reviews table

--- RateMyClasses/wwwroot/js/site.js
+++ RateMyClasses/wwwroot/js/site.js
@@ -2,5 +2,6 @@
 $('#searchDataTable').dataTable({searching: false, ordering: true, info: true});
 $('#popularProfessors').dataTable({searching: false, paging: false, ordering: true});
 $('#reviewList').dataTable({searching: false, paging: false, ordering: true});
+$('#professorReviews').dataTable({searching: false, paging: false, ordering: true});
 $("#makeReviewCourseBox").prop("readonly", true);
 $('#makeReviewCourseBox').val(location.href.match(/([^\/]*)\/*$/)[1]);
