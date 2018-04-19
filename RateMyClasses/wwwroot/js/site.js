bd3340d974fc747a1b2ba91bcaf04549973d529d
Daniel Grigsby <dbg28@case.edu>
2018-04-19 13:00:21 -0400

    Fix sizing, table look-and-feel for popular professors and reviews

--- RateMyClasses/wwwroot/js/site.js
+++ RateMyClasses/wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿// Write your JavaScript code.
 $('#searchDataTable').dataTable({searching: false, ordering: true, info: true});
+$('#popularProfessors').dataTable({searching: false, paging: false, ordering: true});
+$('#reviewList').dataTable({searching: false, paging: false, ordering: true});
 $("#makeReviewCourseBox").prop("readonly", true);
 $('#makeReviewCourseBox').val(location.href.match(/([^\/]*)\/*$/)[1]);
