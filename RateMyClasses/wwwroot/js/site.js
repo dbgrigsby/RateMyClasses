16f4b994ce015206517d2685b3fb2a3a651f6640
Daniel Grigsby <dbg28@case.edu>
2018-04-19 12:33:48 -0400

    Autofill add review course ID box

--- RateMyClasses/wwwroot/js/site.js
+++ RateMyClasses/wwwroot/js/site.js
@@ -1,2 +1,4 @@
 ﻿// Write your JavaScript code.
 $('#searchDataTable').dataTable({searching: false, ordering: true, info: true});
+$("#makeReviewCourseBox").prop("readonly", true);
+$('#makeReviewCourseBox').val(location.href.match(/([^\/]*)\/*$/)[1]);
