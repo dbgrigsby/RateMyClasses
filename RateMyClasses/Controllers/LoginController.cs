01d0f41742793031b7be5dc87cfe057638d0c7f5
Adam Beck <akb93@case.edu>
2018-04-19 09:12:56 -0400

    Fake login screen, general  UI improvements

---
+++ RateMyClasses/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RateMyClasses.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RateMyClasses.Models;
+
+namespace RateMyClasses.Controllers
+{
+	public class LoginController: Controller
+	{
+
+		private readonly CourseContext _context;
+
+		public LoginController(CourseContext context)
+		{
+			_context = context;
+		}
+
+		public ActionResult Index()
+		{ 
+			ViewData["Title"] = "Login";
+			ViewData["Message"] = "Enter your login information to access the moderation queue";
+
+			return View();
+		}
+
+	}
+}
