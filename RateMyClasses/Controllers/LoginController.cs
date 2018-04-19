8be03cadf5095147384bbfe4484192723dd65521
Adam Beck <akb93@case.edu>
2018-04-19 10:51:06 -0400

    Add wrong login redirect

--- RateMyClasses/Controllers/LoginController.cs
+++ RateMyClasses/Controllers/LoginController.cs
@@ -28,12 +28,27 @@ namespace RateMyClasses.Controllers
 			_context = context;
 		}
 
-		public ActionResult Index()
+		public ActionResult Index(Boolean error = false)
 		{ 
 			ViewData["Title"] = "Login";
 			ViewData["Message"] = "Enter your login information to access the moderation queue";
 
+			if (error) {
+				ViewData["Error"] = "Error: Your username or password was incorrect. Please re-enter your login information";
+			}
+
 			return View();
+		}
+
+		public ActionResult Verification(String username, String password)
+		{
+			if (username.ToLower().Equals("adam") && password.ToLower().Equals("beck")) {
+				return RedirectToAction("Index", "Moderator");	
+			}
+			else {
+				return RedirectToAction("Index", new { error = true });
+			}
+
 		}
 
 	}
